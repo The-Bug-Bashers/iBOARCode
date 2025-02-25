d8c05b0ddc170088065364b8e7ab6bb71f3a6323
dagadeta <dagadeta@web.de>
2025-02-25 10:39:12 +0100

    Add Normal-move to Move-motor section

--- api/src/main/resources/static/resources/scripts/buttonSections.js
+++ api/src/main/resources/static/resources/scripts/buttonSections.js
@@ -3,13 +3,16 @@ function showButtons(mode) {
     switch (mode) {
         case "Remote-Control":
             buttonsContainer.innerHTML = getRemoteControlContent();
-            document.getElementById("forwardCheckbox").checked = false;
-            document.getElementById("rightCheckbox").checked = false;
-            document.getElementById("backwardCheckbox").checked = false;
-            document.getElementById("leftCheckbox").checked = false;
+            document.getElementById("forwardRemoteControlCheckbox").checked = false;
+            document.getElementById("rightRemoteControlCheckbox").checked = false;
+            document.getElementById("backwardRemoteControlCheckbox").checked = false;
+            document.getElementById("leftRemoteControlCheckbox").checked = false;
             break;
         case "Move-Motor":
             buttonsContainer.innerHTML = getMoveMotorContent();
+            document.getElementById("value-1").checked = true;
+            document.getElementById("forwardMoveMotorCheckbox").checked = false;
+            document.getElementById("backwardMoveMotorCheckbox").checked = false;
             break;
         default:
             buttonsContainer.innerHTML = "";
@@ -37,31 +40,31 @@ document.addEventListener("keydown", (event) => {
 function getRemoteControlContent() {
     return `
         <h2>Directions</h2>
-        <div id="directionButtons">
+        <div id="directionRemoteControlButtons">
             <div class="directionButtonPlusLabel">
                 <label class="checkbox-container">
-                    <input class="custom-checkbox" checked="" type="checkbox" id="forwardCheckbox">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="forwardRemoteControlCheckbox">
                     <span class="checkmark"></span>
                 </label>
                 <p>Forward</p>
             </div>
             <div class="directionButtonPlusLabel">
                 <label class="checkbox-container">
-                    <input class="custom-checkbox" checked="" type="checkbox" id="rightCheckbox">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="rightRemoteControlCheckbox">
                     <span class="checkmark"></span>
                 </label>
                 <p>Right</p>
             </div>
             <div class="directionButtonPlusLabel">
                 <label class="checkbox-container">
-                    <input class="custom-checkbox" checked="" type="checkbox" id="backwardCheckbox">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="backwardRemoteControlCheckbox">
                     <span class="checkmark"></span>
                 </label>
                 <p>Backward</p>
             </div>
             <div class="directionButtonPlusLabel">
                 <label class="checkbox-container">
-                    <input class="custom-checkbox" checked="" type="checkbox" id="leftCheckbox">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="leftRemoteControlCheckbox">
                     <span class="checkmark"></span>
                 </label>
                 <p>Left</p>
@@ -71,29 +74,50 @@ function getRemoteControlContent() {
         <button id="turnLeftButton" class="button">left</button>
         <button id="turnRightButton" class="button">right</button>
         <h2>Speed</h2>
-        <form id="speedControlForm">
-            <input type="number" min="0" max="100" id="speedControlInput" placeholder="Enter speed here (%)" autocomplete="off">
-            <button id="speedControlSubmit" class="button">Send</button>
+        <form id="speedRemoteControlForm">
+            <input type="number" min="0" max="100" id="speedRemoteControlInput" placeholder="Enter speed here (%)" autocomplete="off">
+            <button id="speedRemoteControlSubmit" class="button">Send</button>
         </form>
     `;
 }
 
 function getMoveMotorContent() {
     return `
-        <div class="radio-input">
-          <label>
-            <input type="radio" id="value-1" name="value-radio" value="value-1" />
-            <span>1</span>
-          </label>
-          <label>
-            <input type="radio" id="value-2" name="value-radio" value="value-2" />
-            <span>2</span>
-          </label>
-          <label>
-            <input type="radio" id="value-3" name="value-radio" value="value-3" />
-            <span>3</span>
-          </label>
-          <span class="selection"></span>
+        <form class="radio-input">
+            <label>
+                <input type="radio" id="value-1" name="value-radio" value="value-1" />
+                <span>1</span>
+            </label>
+            <label>
+                <input type="radio" id="value-2" name="value-radio" value="value-2" />
+                <span>2</span>
+            </label>
+            <label>
+                <input type="radio" id="value-3" name="value-radio" value="value-3" />
+                <span>3</span>
+            </label>
+            <span class="selection"></span>
+        </form>
+        <h2>Normal-move</h2>
+        <form id="speedMoveMotorForm">
+            <input type="number" min="0" max="100" id="speedMoveMotorInput" placeholder="Enter speed here (%)" autocomplete="off">
+            <button id="speedMoveMotorSubmit" class="button">Send</button>
+        </form>
+        <div id="directionMoveMotorButtons">
+            <div class="directionButtonPlusLabel">
+                <label class="checkbox-container">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="forwardMoveMotorCheckbox">
+                    <span class="checkmark"></span>
+                </label>
+                <p>Forward</p>
+            </div>
+            <div class="directionButtonPlusLabel">
+                <label class="checkbox-container">
+                    <input class="custom-checkbox" checked="" type="checkbox" id="backwardMoveMotorCheckbox">
+                    <span class="checkmark"></span>
+                </label>
+                <p>Backward</p>
+            </div>
         </div>
     `;
 }
