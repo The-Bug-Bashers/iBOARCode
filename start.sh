e724750df42dfffcd6762dce4afd5a67fbc28d64
Flottegurke <justus.dicker@web.de>
2025-03-12 13:06:42 +0100

    rename API screen session

--- start.sh
+++ start.sh
@@ -14,20 +14,20 @@ echo -e "${BLUE}Starting iBOAR systems...${NC}"
 echo -e "${BLUE}Starting MQTT broker (mosquitto)...${NC}"
 sudo systemctl start mosquitto
 echo -e "${BLUE}Waiting for MQTT broker to start...${NC}"
-sleep 3
+sleep 1
 
 
 echo -e "${BLUE}Starting REST API (Java Maven Spring Boot)...${NC}"
 cd api || { echo -e "${RED}Failed to enter 'api' directory${NC}"; exit 1; }
 
 # Start Spring Boot in a detached screen session
-sudo screen -dmS springBoot mvn spring-boot:run
+sudo screen -dmS API mvn spring-boot:run
 
 echo -e "${BLUE}Waiting for REST API to start...${NC}"
 sleep 12
 
 # Check if the screen session is still running (using sudo to check root's screen sessions)
-if sudo screen -list | grep -q "springBoot"; then
+if sudo screen -list | grep -q "API"; then
     echo -e "${GREEN}REST API started successfully.${NC}"
 else
     echo -e "${RED}REST API terminated.${NC}"
