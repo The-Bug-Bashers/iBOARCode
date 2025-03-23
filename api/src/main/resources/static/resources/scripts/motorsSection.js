d62b8822912f2861bfb3dbe988d5eb3d2b66cb05
Flottegurke <justus.dicker@web.de>
2025-03-23 23:33:15 +0100

    ajust motors display backround saturation

--- api/src/main/resources/static/resources/scripts/motorsSection.js
+++ api/src/main/resources/static/resources/scripts/motorsSection.js
@@ -6,9 +6,9 @@ combinedChart = new Chart(ctx, {
     data: {
         labels: [],
         datasets: [
-            { label: 'M1 Actual', data: [], borderColor: '#2f6ef6', borderWidth: 2, backgroundColor: 'rgba(7,41,255,0.25)', fill: true, tension: 0.1, pointRadius: 0 },
-            { label: 'M2 Actual', data: [], borderColor: '#d8091f', borderWidth: 2, backgroundColor: 'rgba(255,7,28,0.24)', fill: true, tension: 0.1, pointRadius: 0 },
-            { label: 'M3 Actual', data: [], borderColor: '#0fbc08', borderWidth: 2, backgroundColor: 'rgba(40,255,7,0.3)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M1 Actual', data: [], borderColor: '#2f6ef6', borderWidth: 2, backgroundColor: 'rgb(7,41,255, 0.5)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M2 Actual', data: [], borderColor: '#d8091f', borderWidth: 2, backgroundColor: 'rgba(255,7,28,0.5)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M3 Actual', data: [], borderColor: '#0fbc08', borderWidth: 2, backgroundColor: 'rgba(40,255,7,0.5)', fill: true, tension: 0.1, pointRadius: 0 },
             { label: 'M1 Target', data: [], borderColor: '#84bce3', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
             { label: 'M2 Target', data: [], borderColor: '#e16473', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
             { label: 'M3 Target', data: [], borderColor: '#80cd5d', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
@@ -22,7 +22,7 @@ combinedChart = new Chart(ctx, {
         scales: {
             x: {
                 grid: {
-                    color: '#8e8989',
+                    color: 'rgba(142,137,137,0.5)',
                     display: true,
                 },
                 title: {display: true, text: 'Time'},
@@ -30,7 +30,7 @@ combinedChart = new Chart(ctx, {
             },
             y: {
                 grid: {
-                    color: '#8e8989',
+                    color: 'rgba(142,137,137,0.5)',
                     display: true,
                 },
                 title: {display: true, text: 'Speed (RPM)'},
@@ -39,7 +39,7 @@ combinedChart = new Chart(ctx, {
             },
         },
         animation: {
-            duration: 0
+            duration: 75
         }
     }
 });
