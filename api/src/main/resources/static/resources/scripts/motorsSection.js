5b1b9723691ede8e323da638b99c8b621a1d69e4
Flottegurke <justus.dicker@web.de>
2025-03-23 23:09:32 +0100

    restyle motors display

--- api/src/main/resources/static/resources/scripts/motorsSection.js
+++ api/src/main/resources/static/resources/scripts/motorsSection.js
@@ -1,29 +1,42 @@
 ctx = document.getElementById('combinedChart').getContext('2d');
+Chart.defaults.color = '#ffffff';
+
 combinedChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: [],
         datasets: [
-            { label: 'Motor 1 Actual', data: [], borderColor: 'blue', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0 },
-            { label: 'Motor 1 Target', data: [], borderColor: 'red', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
-            { label: 'Motor 1 Pid', data: [], borderColor: 'red', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] },
-            { label: 'Motor 2 Actual', data: [], borderColor: 'green', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0 },
-            { label: 'Motor 2 Target', data: [], borderColor: 'orange', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
-            { label: 'Motor 2 Pid', data: [], borderColor: 'orange', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] },
-            { label: 'Motor 3 Actual', data: [], borderColor: 'purple', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0 },
-            { label: 'Motor 3 Target', data: [], borderColor: 'pink', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
-            { label: 'Motor 3 Pid', data:
-                    [], borderColor: 'pink', borderWidth: 2, fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] }
+            { label: 'M1 Actual', data: [], borderColor: '#2f6ef6', borderWidth: 2, backgroundColor: 'rgba(7,41,255,0.25)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M2 Actual', data: [], borderColor: '#d8091f', borderWidth: 2, backgroundColor: 'rgba(255,7,28,0.24)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M3 Actual', data: [], borderColor: '#0fbc08', borderWidth: 2, backgroundColor: 'rgba(40,255,7,0.3)', fill: true, tension: 0.1, pointRadius: 0 },
+            { label: 'M1 Target', data: [], borderColor: '#84bce3', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
+            { label: 'M2 Target', data: [], borderColor: '#e16473', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
+            { label: 'M3 Target', data: [], borderColor: '#80cd5d', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [6, 5] },
+            { label: 'M1 Pid', data: [], borderColor: '#5661ed', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] },
+            { label: 'M2 Pid', data: [], borderColor: '#8f113b', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] },
+            { label: 'M3 Pid', data: [], borderColor: '#308a12', borderWidth: 2, backgroundColor: '#43454a', fill: false, tension: 0.1, pointRadius: 0, borderDash: [3, 3] }
         ]
     },
     options: {
         responsive: true,
         scales: {
             x: {
-                title: { display: true, text: 'Time' },
-                ticks: { display: false }
+                grid: {
+                    color: '#8e8989',
+                    display: true,
+                },
+                title: {display: true, text: 'Time'},
+                ticks: {display: false},
             },
-            y: { title: { display: true, text: 'Speed (RPM)' }, suggestedMin: -150, suggestedMax: 150 }
+            y: {
+                grid: {
+                    color: '#8e8989',
+                    display: true,
+                },
+                title: {display: true, text: 'Speed (RPM)'},
+                suggestedMin: -75, suggestedMax: 75
+
+            },
         },
         animation: {
             duration: 0
