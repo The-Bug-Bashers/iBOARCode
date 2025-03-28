b7876d915a2b342bcbbd28c1f8c3ae3f97875a0f
Flottegurke <justus.dicker@web.de>
2025-03-28 19:23:03 +0100

    add NavigatinoService

---
+++ NavigationService/src/main/java/navigation/NavigationService/NavigationServiceApplication.java
@@ -0,0 +1,13 @@
+package navigation.NavigationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationServiceApplication.class, args);
+	}
+
+}
---
+++ NavigationService/src/test/java/navigation/NavigationService/NavigationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package navigation.NavigationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
