2428f8dddb5ebb02888248043d16ff0f9895f793
Flottegurke <justus.dicker@web.de>
2025-03-30 21:05:11 +0200

    add logging

--- NavigationService/src/main/java/navigation/NavigationService/utils/MotorUtils.java
+++ NavigationService/src/main/java/navigation/NavigationService/utils/MotorUtils.java
@@ -4,24 +4,26 @@ import jakarta.annotation.PostConstruct;
 import navigation.NavigationService.ModeHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import navigation.NavigationService.MQTTHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import navigation.NavigationService.utils.AngleUtils;
-
 @Component
 public final class MotorUtils {
+    private static final Logger log = LoggerFactory.getLogger(MQTTHandler.class);
     @Value("${mqtt.channel.motor.drive}") private String MOTOR_DRIVE_CHANNEL;
     @Value("${boar.diameter}") private double BOAR_DIAMETER;
 
     private static String staticMotorDriveChannel;
-    private static double staticBoarDiameter;
+    private static double boarRadius;
     @PostConstruct
     public void init() {
         staticMotorDriveChannel = MOTOR_DRIVE_CHANNEL;
-        staticBoarDiameter = BOAR_DIAMETER;
+        boarRadius = BOAR_DIAMETER / 2.d;
+        log.info("Boar radius: {}", boarRadius);
     }
 
     public static void stopMotors() {
@@ -31,11 +33,12 @@ public final class MotorUtils {
     public static double calculateMaxDrivingDistance(double targetAngle) {
         JSONArray data = ModeHandler.getLatestLidarData();
         if (data == null) return 14;
-        double maxDrivingDistance = 14 - staticBoarDiameter / 2;
+        double maxDrivingDistance = 14 - boarRadius;
 
         final double[] checkAngles = {AngleUtils.normalizeAngle(targetAngle - 90), AngleUtils.normalizeAngle(targetAngle + 90)};
         final double minCheckAngle = Math.min(checkAngles[0], checkAngles[1]);
         final double maxCheckAngle = Math.max(checkAngles[0], checkAngles[1]);
+        log.info("targetAngle: {}, maxCheckAngle: {}, minCheckAngle: {}", targetAngle, maxCheckAngle, minCheckAngle);
 
         for (int i = 0; i < data.length(); i++) {
             JSONObject obj = data.getJSONObject(i);
@@ -47,9 +50,9 @@ public final class MotorUtils {
             double deviationAngle = Math.abs(currentAngle - maxCheckAngle);
             double currentHalfWidth = Math.sin(Math.toRadians(deviationAngle)) * currentDistance;
 
-            if(currentHalfWidth > staticBoarDiameter / 2) continue;
+            if(currentHalfWidth > boarRadius) continue;
 
-            double currentMaxDrivingDistance = Math.cos(Math.toRadians(deviationAngle)) * currentDistance - staticBoarDiameter / 2;
+            double currentMaxDrivingDistance = Math.cos(Math.toRadians(deviationAngle)) * currentDistance - boarRadius;
             if (currentMaxDrivingDistance < maxDrivingDistance) maxDrivingDistance = currentMaxDrivingDistance;
         }
         return maxDrivingDistance;
